0e6389d259e3d5c25a1b845b8039ea13b5ff5689
Oliver Caldwell <olliec87@gmail.com>
2013-10-03 23:13:04 +0100

    Added more key combo tests.

--- tests/index.js
+++ tests/index.js
@@ -30,3 +30,67 @@ test('handling a key with an exact match executes the right function', function 
 
 	engine.handleKey('<w>');
 });
+
+test('sending one key when there are other potential matches does not execute', function (t) {
+	t.plan(1);
+	var engine = new MoedCore();
+
+	engine.mapKeys('<w>', 'normal', function () {
+		t.fail('<w> should do nothing because it is ambiguous');
+	});
+
+	engine.mapKeys('<w><b>', 'normal', function () {
+		t.fail('<w><b> should not be executed because this was a single key');
+	});
+
+	engine.mapKeys('<b>', 'normal', function () {
+		t.pass('<b> is the only one that should execute');
+	});
+
+	engine.handleKey('<b>');
+	engine.handleKey('<w>');
+});
+
+test('can register and use key combinations', function (t) {
+	t.plan(1);
+	var engine = new MoedCore();
+
+	engine.mapKeys('<w>', 'normal', function () {
+		t.fail('<w> should do nothing because it is ambiguous');
+	});
+
+	engine.mapKeys('<w><b>', 'normal', function () {
+		t.pass('<w><b> should execute this');
+	});
+
+	engine.mapKeys('<b>', 'normal', function () {
+		t.fail('<b> should not execute when not called');
+	});
+
+	engine.handleKey('<w>');
+	engine.handleKey('<b>');
+});
+
+test('can let a key combination timeout', function (t) {
+	t.plan(1);
+	var engine = new MoedCore();
+	engine.keyCombinationTimeout = 50;
+
+	engine.mapKeys('<w>', 'normal', function () {
+		t.fail('<w> should do nothing because it is ambiguous');
+	});
+
+	engine.mapKeys('<w><b>', 'normal', function () {
+		t.fail('<w><b> should not execute because it times out');
+	});
+
+	engine.mapKeys('<b>', 'normal', function () {
+		t.pass('<b> should execute because the combo times out');
+	});
+
+	engine.handleKey('<w>');
+
+	setTimeout(function () {
+		engine.handleKey('<b>');
+	}, 100);
+});
