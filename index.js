b2840773175515324aae581477003bcbe90c31a3
Oliver Caldwell <olliec87@gmail.com>
2013-10-03 21:26:01 +0100

    JSDoc and shuffle of source.

--- index.js
+++ index.js
@@ -12,13 +12,25 @@ function MoedCore() {
 	this.sourceFile(path.join(__dirname, 'lib/config.js'));
 }
 
+/**
+ * Parses the provided JavaScript in the engines context. It allows it to
+ * register functions and settings to share with all other sourced code.
+ *
+ * @param {String} code A chunk of JavaScript to source.
+ */
+MoedCore.prototype.source = function (code) {
+	vm.runInContext(code, this.context);
+};
+
+/**
+ * Does exactly what `source` does, loads JavaScript into the engines context,
+ * but must be provided with a file name to read the code from.
+ *
+ * @param {String} path The file to load and source.
+ */
 MoedCore.prototype.sourceFile = function (path) {
 	var code = fs.readFileSync(path);
 	this.source(code);
-};
-
-MoedCore.prototype.source = function (code) {
-	vm.runInContext(code, this.context);
 };
 
 module.exports = MoedCore;
