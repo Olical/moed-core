e10f1d91943c5e850675f3c90a7859564118fa1a
Oliver Caldwell <olliec87@gmail.com>
2013-10-04 23:48:01 +0100

    Added the settings module and tested.

---
+++ modules/settings.js
@@ -0,0 +1,76 @@
+/**
+ * The settings module stores key value pairs. You can pass a target or context
+ * string to add context aware overrides. This allows you to set a global
+ * setting or an override for, say, a file type.
+ *
+ * @class
+ */
+function Settings() {
+	this._settings = {};
+}
+
+/**
+ * Fetches a key from the settings with an optional target context.
+ *
+ * @param {String} key Dot delimited object path for the setting.
+ * @param {String} [target] Optional context for the setting, defaults to "default".
+ * @return {*} Either the matched value or undefined if not found.
+ */
+Settings.prototype.get = function (key, target) {
+	var settings = this._resolveTarget(target);
+
+	if (typeof settings[key] === 'undefined' && target !== 'default') {
+		settings = this._resolveTarget('default');
+	}
+
+	return settings[key];
+};
+
+/**
+ * Sets a key in the settings to the specified value with an optional target
+ * context.
+ *
+ * @param {String} key Dot delimited object path for the setting.
+ * @param {*} value This is what will be set.
+ * @param {String} [target] Optional context for the setting, defaults to "default".
+ */
+Settings.prototype.set = function (key, value, target) {
+	var settings = this._resolveTarget(target);
+	settings[key] = value;
+};
+
+/**
+ * Removes a key from the settings with an optional target context.
+ *
+ * @param {String} key Dot delimited object path for the setting.
+ * @param {String} [target] Optional context for the setting, defaults to "default".
+ */
+Settings.prototype.remove = function (key, target) {
+	var settings = this._resolveTarget(target);
+	delete settings[key];
+};
+
+/**
+ * Resolves the target context name to an object.
+ *
+ * @param {String} target Context to resolve.
+ * @return {Object} The context object you requested.
+ */
+Settings.prototype._resolveTarget = function (target) {
+	if (!target) {
+		target = 'default';
+	}
+
+	if (!this._settings[target]) {
+		this._settings[target] = {};
+	}
+
+	return this._settings[target];
+};
+
+module.exports = {
+	Settings: Settings,
+	create: function (context) {
+		context.settings = new Settings();
+	}
+};
---
+++ tests/modules/settings.js
@@ -0,0 +1,44 @@
+var test = require('tape');
+var Settings = require('../../modules/settings.js').Settings;
+
+test('getting undefined is undefined', function (t) {
+	t.plan(1);
+	var s = new Settings();
+	var result = s.get('this.is.undefined');
+	t.strictEqual(typeof result, 'undefined', 'undefined setting returns undefined');
+});
+
+test('can set and get values', function (t) {
+	t.plan(2);
+	var s = new Settings();
+	s.set('foo', 100);
+	s.set('bar', 200);
+
+	t.strictEqual(s.get('foo'), 100, 'foo is correct');
+	t.strictEqual(s.get('bar'), 200, 'bar is correct');
+});
+
+test('can remove keys', function (t) {
+	t.plan(1);
+	var s = new Settings();
+	s.set('foo', 100);
+	s.remove('foo');
+	t.strictEqual(typeof s.get('foo'), 'undefined', 'foo was deleted');
+});
+
+test('can override settings per a special target string', function (t) {
+	t.plan(2);
+	var s = new Settings();
+	s.set('foo', 100);
+	s.set('foo', 200, 'mode.insert');
+	t.strictEqual(s.get('foo', 'mode.insert'), 200, 'overrode');
+	s.remove('foo', 'mode.insert');
+	t.strictEqual(s.get('foo'), 100, 'reset');
+});
+
+test('falls back to default if no special target is found', function (t) {
+	t.plan(1);
+	var s = new Settings();
+	s.set('foo', 100);
+	t.strictEqual(s.get('foo', 'mode.insert'), 100, 'defaulted');
+});
