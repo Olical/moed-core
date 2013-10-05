bf8694c1a48808f12459ae7cf6cdc9ca90b9c465
Oliver Caldwell <olliec87@gmail.com>
2013-10-05 18:19:49 +0100

    Basic buffer creation and retrieval.

--- modules/buffers.js
+++ modules/buffers.js
@@ -6,10 +6,42 @@
  */
 function Buffers(context) {
 	this._context = context;
+	this._uniqueBufferIdentifier = 0;
+	this._buffers = {};
 }
+
+/**
+ * Creates a new buffer and returns it's identifier.
+ *
+ * @param {String} [initialContent] The default string to store within the buffer. Defaults to an empty string.
+ * @return {Number} The new buffers identifier.
+ */
+Buffers.prototype.create = function (initialContent) {
+	this._uniqueBufferIdentifier += 1;
+
+	this._buffers[this._uniqueBufferIdentifier] = {
+		content: initialContent,
+		cursor: {
+			x: 0,
+			y: 0
+		}
+	};
+
+	return this._uniqueBufferIdentifier;
+};
+
+/**
+ * Fetches a buffer object by it's identifier.
+ *
+ * @param {Number} identifier Identification number of your desired buffer.
+ * @return {Object} The requested buffer.
+ */
+Buffers.prototype.get = function (identifier) {
+	return this._buffers[identifier];
+};
 
 module.exports = {
 	register: function (context) {
-		exports.buffers = new Buffers(context);
+		context.buffers = new Buffers(context);
 	}
 };
--- tests/modules/buffers.js
+++ tests/modules/buffers.js
@@ -1,2 +1,12 @@
 var test = require('tape');
 var MoedCore = require('../..');
+
+test('can create and fetch a buffer', function (t) {
+	t.plan(1);
+	var engine = new MoedCore();
+	var b = engine.context.buffers;
+	var content = 'this is the content';
+
+	var id = b.create(content);
+	t.strictEqual(b.get(id).content, content, 'content was retrieved');
+});
