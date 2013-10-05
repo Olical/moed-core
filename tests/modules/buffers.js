c439032176a3ed7e644d37402a92af5a04bb6379
Oliver Caldwell <olliec87@gmail.com>
2013-10-05 18:37:53 +0100

    Made the basic buffers test use multiple lines.

--- tests/modules/buffers.js
+++ tests/modules/buffers.js
@@ -5,7 +5,7 @@ test('can create and fetch a buffers contents', function (t) {
 	t.plan(1);
 	var engine = new MoedCore();
 	var b = engine.context.buffers;
-	var content = 'this is the content';
+	var content = 'this is the content\non two lines!';
 
 	var id = b.create(content);
 	t.strictEqual(b.getContent(id), content, 'content was retrieved');
