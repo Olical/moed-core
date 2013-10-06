6acadb3ce1e77c4b547e03706962a867dc537e9b
Oliver Caldwell <olliec87@gmail.com>
2013-10-06 12:55:50 +0100

    Added basic creation and tests for windows.

--- modules/windows.js
+++ modules/windows.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 /**
  * A window is almost the equivalent of a view, if a buffer is a model. It can
  * run side by side with other windows, this is what the frontend will display.
@@ -7,7 +9,32 @@
  */
 function Windows(context) {
 	this._context = context;
+	this._windows = {};
 }
+
+/**
+ * Creates a new window with a buffer.
+ *
+ * @param {String} buffer The identifier for the buffer the window should point at.
+ * @return {String} The identifier for the window.
+ */
+Windows.prototype.create = function (buffer) {
+	var identifier = _.uniqueId('window_');
+	this._windows[identifier] = {
+		buffer: this._context.buffers.get(buffer)
+	};
+	return identifier;
+};
+
+/**
+ * Fetches the window object by identifier.
+ *
+ * @param {String} identifier The window you want to retrieve.
+ * @return {Object} The raw window object you requested.
+ */
+Windows.prototype.get = function (identifier) {
+	return this._windows[identifier];
+};
 
 module.exports = {
 	register: function (context) {
--- tests/modules/windows.js
+++ tests/modules/windows.js
@@ -1,2 +1,16 @@
 var test = require('tape');
 var MoedCore = require('../..');
+
+test('can create a window that contains a buffer', function (t) {
+	t.plan(1);
+	var engine = new MoedCore();
+	var file = 'Hello, World!';
+	var w = engine.context.windows;
+	var b = engine.context.buffers;
+
+	var bid = b.create(file);
+	var wid = w.create(bid);
+	var win = w.get(wid);
+
+	t.strictEqual(win.buffer.lines[0], file, 'window contained the buffer');
+});
