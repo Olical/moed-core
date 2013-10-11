45d7eb3e12941fd69d0a216ff6d19d5239c2be58
Oliver Caldwell <olliec87@gmail.com>
2013-10-11 22:48:16 +0100

    Added cursor movement command.

--- modules/commands.js
+++ modules/commands.js
@@ -8,6 +8,17 @@ function Commands(context) {
 	this._context = context;
 }
 
+/**
+ * Updates the specified window's cursor position.
+ *
+ * @param {Object} win
+ * @param {Object} position
+ */
+Commands.prototype.moveCursor = function (win, position) {
+	win.cursor.x = position.x;
+	win.cursor.y = position.y;
+};
+
 module.exports = {
 	register: function (context) {
 		context.commands = new Commands(context);
--- tests/modules/commands.js
+++ tests/modules/commands.js
@@ -3,8 +3,24 @@ var MoedCore = require('../..');
 
 function setup() {
 	var engine = new MoedCore();
+	var s = engine.context.settings;
+	var win = s.get('windows.current');
 
 	return {
-		engine: engine
+		engine: engine,
+		c: engine.context.commands,
+		s: s,
+		win: win // :D
 	};
 }
+
+test('can move the cursor to the specified position', function (t) {
+	t.plan(2);
+	var position = {
+		x: 5,
+		y: 0
+	};
+	this.c.moveCursor(this.win, position);
+	t.strictEqual(this.win.cursor.x, position.x, 'x is correct');
+	t.strictEqual(this.win.cursor.y, position.y, 'y is correct');
+}.bind(setup()));
