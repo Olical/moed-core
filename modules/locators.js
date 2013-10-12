a4cb02064aa7c9d1073bde9a6d4fa935b232e41c
Oliver Caldwell <olliec87@gmail.com>
2013-10-12 11:49:23 +0100

    Added basic locators.
    These move the cursor left, right, up and down.

--- modules/locators.js
+++ modules/locators.js
@@ -10,6 +10,42 @@ function Locators(context) {
 	this._context = context;
 }
 
+/**
+ * Moves the windows cursor left.
+ *
+ * @param {Object} win
+ */
+Locators.prototype.left = function (win) {
+	win.cursor.x -= 1;
+};
+
+/**
+ * Moves the windows cursor right.
+ *
+ * @param {Object} win
+ */
+Locators.prototype.right = function (win) {
+	win.cursor.x += 1;
+};
+
+/**
+ * Moves the windows cursor up.
+ *
+ * @param {Object} win
+ */
+Locators.prototype.up = function (win) {
+	win.cursor.y -= 1;
+};
+
+/**
+ * Moves the windows cursor down.
+ *
+ * @param {Object} win
+ */
+Locators.prototype.down = function (win) {
+	win.cursor.y += 1;
+};
+
 module.exports = {
 	register: function (context) {
 		context.locators = new Locators(context);
--- tests/modules/locators.js
+++ tests/modules/locators.js
@@ -4,9 +4,35 @@ var MoedCore = require('../..');
 function setup() {
 	var engine = new MoedCore();
 	var l  = engine.context.locators;
+	var win = engine.context.settings.get('windows.current');
 
 	return {
 		engine: engine,
-		l: l
+		l: l,
+		win: win,
 	};
 }
+
+test('move left', function (t) {
+	t.plan(1);
+	this.l.left(this.win);
+	t.strictEqual(this.win.cursor.x, -1, 'moved the cursor left');
+}.bind(setup()));
+
+test('move right', function (t) {
+	t.plan(1);
+	this.l.right(this.win);
+	t.strictEqual(this.win.cursor.x, 1, 'moved the cursor right');
+}.bind(setup()));
+
+test('move up', function (t) {
+	t.plan(1);
+	this.l.up(this.win);
+	t.strictEqual(this.win.cursor.y, -1, 'moved the cursor up');
+}.bind(setup()));
+
+test('move left', function (t) {
+	t.plan(1);
+	this.l.down(this.win);
+	t.strictEqual(this.win.cursor.y, 1, 'moved the cursor down');
+}.bind(setup()));
